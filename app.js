8c591c775cab7b7379d55489acf3c8a905a85132
unknown <sirmario42@yahoo.com>
2024-11-30 04:22:33 +0100

    set headers

--- app.js
+++ app.js
@@ -12,12 +12,24 @@ app.use(
     credentials: true,
   })
 );
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https") {
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    } else {
+      next();
+    }
+  });
+}
 mongoose
   .connect(Db_URL)
   .then((conn) => {
     if (conn != null) console.log("connection established.");
   })
   .catch((err) => console.log(err));
+app.get("/", (req, res) => {
+  res.send("<h1> Welcome to Task Master API");
+});
 app.use("/api/v1/task", taskRoute);
 const port = process.env.PORT || 5000;
 app.listen(port, () => `server running on port ${port}`);
